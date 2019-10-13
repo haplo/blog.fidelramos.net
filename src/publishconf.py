5756815a274e7f51c1a357c77fe98e9746bdf37e
Fidel Ramos <f@fidelramos.net>
2019-10-13 22:26:40 +0000

    Category feeds better than tag feeds

--- src/publishconf.py
+++ src/publishconf.py
@@ -15,7 +15,7 @@ SITEURL = 'https://blog.fidelramos.net'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-TAG_FEED_ATOM = 'feeds/{slug}.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 TRANSLATION_FEED_ATOM = 'feeds/{lang}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
