5fb711e2eaee287eb0e8d77fbd82df31dac12abf
Fidel Ramos <f@fidelramos.net>
2019-10-19 20:38:24 +0000

    Flex theme

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -4,14 +4,27 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Fidel Ramos'
 SITENAME = 'blog.fidelramos.net'
+SITETITLE = AUTHOR
+SITESUBTITLE = ''
+SITEDESCRIPTION = 'Fidel Ramos personal blog'
 SITEURL = ''
+SITELOGO = SITEURL + '/images/fidel.jpg'
+FAVICON = SITEURL + '/images/favicon.ico'
+
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-sa'
+}
+COPYRIGHT_YEAR = 2019
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Madrid'
 
-DEFAULT_LANG = 'en'
+DEFAULT_DATE = 'fs'
 
+LOAD_CONTENT_CACHE = False
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,19 +33,43 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-# LINKS = (
-#     ('Pelican', 'http://getpelican.com/'),
-# )
-
-# Social widget
-SOCIAL = (
-    ('Twitter', 'https://twitter.com/ampajaro'),
-    ('Mastodon', ''),
-    ('Flickr', ''),
+# Main menu, above the article
+MAIN_MENU = True
+MENUITEMS = (
+    ('Archive', '/archives#archives'),
+    ('Categories', '/categories#categories'),
+    ('Tags', '/tags#tags'),
 )
 
-DEFAULT_PAGINATION = False
+LINKS = (
+    ('Archive', '/archives#archives'),
+    ('Categories', '/categories#categories'),
+    ('Tags', '/tags#tags'),
+    ('DuckDuckGo', 'https://duckduckgo.com')
+)
+
+# Social widget
+GITHUB_URL = 'https://github.com/haplo'
+SOCIAL = (
+    ('twitter', 'https://twitter.com/ampajaro'),
+#    ('mastodon', ''),
+    ('github', GITHUB_URL),
+    ('flickr', 'https://www.flickr.com/photos/fidelramos/'),
+    ('linkedin', 'https://www.linkedin.com/in/fidelramos/'),
+    ('rss', SITEURL+'/atom.xml'),
+)
+
+TWITTER_USERNAME = 'ampajaro'
+
+DEFAULT_PAGINATION = 10
+
+ROBOTS = 'index,follow'
+
+THEME = '../../pelican-theme-Flex'
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/custom.css'},
+}
+CUSTOM_CSS = 'static/custom.css'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -44,6 +81,25 @@ JINJA_ENVIRONMENT = {
     'extensions': ['jinja2.ext.i18n'],
 }
 
+# i18n and language configuration
+DEFAULT_LANG = 'en'
+OG_LOCALE = 'en_US'
+LOCALE = 'en_US'
+
 I18N_SUBSITES = {
-    'es': {},
+    'es': {
+        'INDEX_DESCRIPTION': 'Blog personal de Fidel Ramos',
+        'OG_LOCALE': 'es_ES',
+        'LOCALE': 'es_ES',
+        'MENUITEMS': (
+            ('Archivo', SITEURL+'/es/archives#archives'),
+            ('Categorías', SITEURL+'/es/categories#categories'),
+            ('Etiquetas', SITEURL+'/es/tags#tags'),
+        ),
+    },
+}
+
+DATE_FORMATS = {
+    'en': '%Y-%m-%d',
+    'es': '%Y-%m-%d',
 }
