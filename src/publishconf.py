e366c33d1fe9b83ee3d1bfba0197a0015c768937
Fidel Ramos <f@fidelramos.net>
2022-05-24 22:35:03 +0100

    Add shynet tracking

--- src/publishconf.py
+++ src/publishconf.py
@@ -18,3 +18,5 @@ TRANSLATION_FEED_ATOM = 'feeds/{lang}.atom.xml'
 TRANSLATION_FEED_RSS = 'feeds/{lang}.rss.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+
+SHYNET_URL = 'https://shynet.fidelramos.net/ingress/c91d9018-d2d1-4197-b607-ab0aa20fdd32'
